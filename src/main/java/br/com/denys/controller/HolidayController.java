875f712553f675ff7afb429f81b4ef6e04fd3191
Denys Maiolli <denysmaiolli@hotmail.com>
2023-08-18 23:17:52 -0300

    feat: Adicionando busca de feriados por ano

--- src/main/java/br/com/denys/controller/HolidayController.java
+++ src/main/java/br/com/denys/controller/HolidayController.java
@@ -4,6 +4,7 @@ import br.com.denys.model.Holiday;
 import br.com.denys.service.HolidayService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -17,6 +18,7 @@ public class HolidayController {
 
     private final HolidayService service;
 
+    @GetMapping(value = "year/{year}")
     public ResponseEntity<List<Holiday>> forYear(@PathVariable int year) {
         return ResponseEntity.ok(service.ofYear(year));
     }
